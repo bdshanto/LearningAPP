d919edab0101ffe8961666584e292199c0ab9271
SHaDoW <hsib@yahoo.com>
2019-12-19 13:43:28 +0600

    API controll and json view in row

---
+++ Ecommerce.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ecommerce.Library.Models;
+using Ecommerce.Library.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Web.Controllers.API
+{
+    [Route("api/products")]
+    public class ProductsController : ControllerBase
+    {
+        UnityOfWork _unityOfWork;
+
+        public ProductsController()
+        {
+            _unityOfWork = new UnityOfWork();
+        }
+
+
+        // GET
+
+        public IEnumerable<dynamic> Get()
+        {
+
+            var products = _unityOfWork
+                .ProductRepository
+                .GetAll()
+                .Select(c => new
+                {
+                    c.Id,
+                    c.Name,
+                    c.Price,
+                    Shop = new Shop
+                    {
+                        Id = c.Id,
+                        Name = c.Name,
+                    }
+                });
+
+
+
+            return products.ToList();
+        }
+    }
+}
